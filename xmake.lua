3eef1f8d8490dae1b5d4e52b95ecc313e3d9931b
dijunkun <junkun.di@hotmail.com>
2023-11-29 22:57:03 -0800

    Disable ffmpeg openh264 codec

--- xmake.lua
+++ xmake.lua
@@ -11,8 +11,8 @@ add_defines("ASIO_STANDALONE", "ASIO_HAS_STD_TYPE_TRAITS", "ASIO_HAS_STD_SHARED_
     "ASIO_HAS_STD_ADDRESSOF", "ASIO_HAS_STD_ATOMIC", "ASIO_HAS_STD_CHRONO", 
     "ASIO_HAS_CSTDINT", "ASIO_HAS_STD_ARRAY",  "ASIO_HAS_STD_SYSTEM_ERROR")
 
-add_requires("asio 1.24.0", "nlohmann_json", "spdlog 1.11.0", "openfec", "libopus 1.4", "sdl2")
-add_packages("asio", "nlohmann_json", "spdlog", "openfec", "libopus", "sdl2")
+add_requires("asio 1.24.0", "nlohmann_json", "spdlog 1.11.0", "openfec", "libopus 1.4")
+add_packages("asio", "nlohmann_json", "spdlog", "openfec", "libopus")
 
 includes("thirdparty")
 
@@ -267,10 +267,10 @@ target("projectx")
 --     add_files("tests/fec/simple_server.cpp")
 --     add_includedirs("tests/fec")
 
-target("opus_test")
-    set_kind("binary")
-    add_packages("libopus", "sdl2")
-    add_files("tests/opus/OpusEncoderImpl.cpp",
-        "tests/opus/OpusDecoderImpl.cpp",
-        "tests/opus/main.cpp")
-    add_includedirs("tests/opus")
+-- target("opus_test")
+--     set_kind("binary")
+--     add_packages("libopus", "sdl2")
+--     add_files("tests/opus/OpusEncoderImpl.cpp",
+--         "tests/opus/OpusDecoderImpl.cpp",
+--         "tests/opus/main.cpp")
+--     add_includedirs("tests/opus")
